bc7354bd47ca325db7d91e8fc30f4117f256b805
Reinaldo silva <tbx800@gmail.com>
2020-05-07 16:59:25 -0300

    mais alguns

---
+++ Exercícios/008.py
@@ -0,0 +1,8 @@
+m = float(input('Digite uma medida em metros:'))
+print('A medida de {}m corresponde a:'.format(m))
+print('{}km.'.format(m/1000))
+print('{}hm.'.format(m/100))
+print('{}dam.'.format(m/10))
+print('{}dm.'.format(m*10))
+print('{}cm.'.format(m*100))
+print('{}mm.'.format(m*1000))
